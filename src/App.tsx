f1fa650a069024d5fad70b45c241036200614784
Sam Fentress <sfentress@concord.org>
2019-03-04 16:47:36 -0500

    Add CodapInterface, iframe phone, and init plugin
    This adds a typescript conversion of
    
    https://github.com/concord-consortium/codap-data-interactives/blob/master/Common/js/CodapInterface.js
    
    This is intended to be a straight refactoring that passes ts-lint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import codapInterface from './lib/CodapInterface';
 
 class App extends Component {
+  componentWillMount() {
+    const interfaceConfig = {
+      name: "Sample Plugin",
+      dimensions: {
+        width: 200,
+        height: 200
+      },
+      version: "0.0.1",
+      customInteractiveStateHandler: true
+    };
+
+    codapInterface.init(interfaceConfig);
+  }
+
   render() {
     return (
       <div className="App">
