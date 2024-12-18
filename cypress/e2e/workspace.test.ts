446a0cc39e3ff07959a5ed9f9cc059a0107d14ce
Teale Fristoe <fristoe@gmail.com>
2024-12-18 08:30:07 -0800

    Update dependencies and cypress test.

--- cypress/e2e/workspace.test.ts
+++ cypress/e2e/workspace.test.ts
@@ -7,7 +7,9 @@ context("Test the overall app", () => {
 
   describe("Desktop functionalities", () => {
     it("renders with text", () => {
-      ae.getApp().should("have.text", "Hello World");
+      // Since the cypress tests are not running in CODAP, the plugin will not receive responses to API requests,
+      // which might cause errors that will display in an overlay, failing the tests.
+      ae.getApp().should("contain.text", "CODAP Starter Plugin");
     });
   });
 });
